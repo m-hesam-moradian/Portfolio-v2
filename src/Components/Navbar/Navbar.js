da311f984ef3d8c37aced1407a15cfefc62aef2b
m.hesam moradian <m.hesam.moradian@gmail.com>
2023-11-29 19:29:23 +0330

    nav Component done

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import "./Navbar.css";
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      {" "}
+      <header>
+        <a href="#" class="logo">
+          <img src="/img/logos/logo-removebg-preview.png" alt="" />
+        </a>
+        <div class="bx bx-menu" id="menu-icon"></div>
+        <ul class="navbar">
+          <li>
+            <a href="#home">home</a>
+          </li>
+          <li>
+            <a href="#about">about me</a>
+          </li>
+          <li>
+            <a href="#services">services</a>
+          </li>
+          <li>
+            <a href="#portfolio">portfolio</a>
+          </li>
+          <li>
+            <a href="#contact">contact</a>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
 }
