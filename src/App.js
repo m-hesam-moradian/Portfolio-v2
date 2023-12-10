001abe38cb720d5863fe5b8d529f228a2f4e21d8
m.hesam moradian <m.hesam.moradian@gmail.com>
2023-12-10 17:00:53 +0330

    faild

--- src/App.js
+++ src/App.js
@@ -1,13 +1,79 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Index from "./Pages/Index/Index";
-
+import React from "react"
+import Particles from 'react-tsparticles'
+import { loadFull } from "tsparticles";
+ 
 function App() {
+  const particlesInit = async (main) => {
+    console.log(main);
+ 
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+ 
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
     <div className="App">
-      <Index />
-    </div>
+   
+     <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+              options={{
+            background: {
+              color: '#162747',
+            },
+            fpsLimit: 40,
+            interactivity: {
+              detectsOn: 'canvas',
+              events: {
+                resize: true
+              },
+            },
+            particles: {
+              color: {
+                value: "#f1f1f1"
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 1080
+                },
+                limit: 0,
+                value: 500,
+              },
+              opacity: {
+                animation: {
+                  enable: true,
+                  minimumValue: 0.5,
+                  speed: 1,
+                  sync: false,
+                },
+                random: {
+                  enable: true,
+                  minimumValue: 0.1,
+                },
+                value: 1,
+              },
+              shape: {
+                type: 'circle',
+       
+              },
+              size: {
+                random: {
+                  enable: true,
+                  minimumValue: 0.5
+                },
+                value: 1
+              }
+            }
+          }}
+      />  
+  </div>
   );
 }
-
+ 
 export default App;
